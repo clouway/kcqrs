5213c1f0c2d579add60ebc121cd291f4a468c804
Miroslav Genov <mgenov@gmail.com>
2018-04-12 16:37:21 +0300

    kcqrs-client: pass security token using custom initializers
    Code which is using the GsonHttpKCQRSClient is now able to add
    additional attributes to the request which are send to the target server
    using the hooked GsonHttpKCQRSClient.
    
    This interceptor is use-full when security tokens need to be passed to
    the target backend.

---
+++ kcqrs-client-gson/src/main/kotlin/com/clouway/kcqrs/client/gson/NopHttpRequestInitializer.kt
@@ -0,0 +1,17 @@
+package com.clouway.kcqrs.client.gson
+
+import com.google.api.client.http.HttpRequest
+import com.google.api.client.http.HttpRequestInitializer
+
+/**
+ * NopHttpRequestInitializer is an initializer which is doing nothing and is used when
+ * client code doesn't have concrete one set.
+ * 
+ * @author Miroslav Genov (miroslav.genov@clouway.com)
+ */
+internal class NopHttpRequestInitializer : HttpRequestInitializer {
+    override fun initialize(request: HttpRequest) {
+        // nop
+    }
+
+}
